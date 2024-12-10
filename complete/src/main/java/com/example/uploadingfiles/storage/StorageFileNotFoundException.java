3cb10fec9e94a85ecfa25a0e513065603f66a8b9
Jun-Gyu Lee <yjpress@hanmail.net>
2024-12-10 10:23:35 +0900

    super

--- complete/src/main/java/com/example/uploadingfiles/storage/StorageFileNotFoundException.java
+++ complete/src/main/java/com/example/uploadingfiles/storage/StorageFileNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.uploadingfiles.storage;
 public class StorageFileNotFoundException extends StorageException {
 
 	public StorageFileNotFoundException(String message) {
-		super(message);
+		super(message);//super 입니다
 	}
 
 	public StorageFileNotFoundException(String message, Throwable cause) {
